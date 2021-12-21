c60d2fedd3d2ccf789562300627e9ae01c054fb3
Reinaldo Silva Mejia <reinaldo.silva@webbeds.com>
2021-12-21 23:17:10 +0100

    first commit

---
+++ Challenge/Challenge/Controllers/HotelController.cs
@@ -0,0 +1,29 @@
+﻿using Challenge.Services;
+using Microsoft.AspNetCore.Mvc;
+using PruebaApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge.Controllers
+{
+    [Route ("api/[controller]")]
+    public class HotelController : Controller
+    {
+        private readonly IHotelService _hotelService;
+
+        public HotelController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+
+        // TODO: pasar los valores por params
+        [HttpGet]
+        public async Task<List<Record>> GetHotels()
+        {
+            return await _hotelService.GetHotels();
+        }
+    }
+}
